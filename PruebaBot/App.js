89e141e75defe671acebc5e4c663e8327479864c
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-07-10 12:01:18 -0300

    falta input y shadow en box

--- PruebaBot/App.js
+++ PruebaBot/App.js
@@ -8,7 +8,7 @@ const TextInputExample = () => {
   const [vuelos, setVuelos] = React.useState('');
   const [loading, setLoading] = React.useState(false)
   const substr = 'flight';
-
+  
   React.useEffect( () => {
     console.log(number.includes(substr));
     if(number.toLowerCase().includes(substr.toLowerCase())){
@@ -22,16 +22,16 @@ const TextInputExample = () => {
 
 
   return (
-    <View>
+    <View style={[styles.container, styles.shadowProp]}>
+      <View style={styles.box}>
+      <Text style={styles.text2}>Ask for your flight</Text>
       <TextInput
               style={styles.input}
               onChangeText={setNumber}
-              value={number}
-              placeholder="Ask for your flight"
-              keyboardType="numeric"
-      />
+              value={number}      />
+
       {!loading ? (
-        <Text>Loading...</Text>
+        <Text style={styles.text}>Loading...</Text>
       ) : (
         vuelos.map((obj) => {
           return (
@@ -43,17 +43,57 @@ const TextInputExample = () => {
         })
         
       )} 
+      </View>
+      
 
     </View>
     );
   }
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    //backgroundColor: `#4169e1`,
+    backgroundColor: '#fffafa',
+  },
   input: {
     height: 40,
+    width: 300,
     margin: 12,
     borderWidth: 1,
     padding: 10,
+    backgroundColor: `#fffafa`,
+    marginTop: 50,
   },
+  text: {
+    height: 40,
+    padding: 10,
+    color: 'black',
+  },
+  text2: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  box: {
+    borderColor: 'black',
+    borderWidth:  1,
+    padding: 10,
+    backgroundColor: 'white',
+    height: 400,
+      
+  
+  },
+  shadowProp: {
+    shadowColor: '#171717',
+    shadowOffset: {width: -2, height: 4},
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+
+
+  
 });
 
 export default TextInputExample;
